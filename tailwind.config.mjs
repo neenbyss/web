e5b18736f6949d31572d2cc1c8d905b10c9d0d80
Kamerr Ezz <kamerr_oficial@hotmail.com>
2024-05-16 19:04:29 -0600

    feat: nuevo diseño (aun falta)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"]
+        poppins: ["outfit", "sans-serif"]
       },
       colors: {
         "electric-violet": {
